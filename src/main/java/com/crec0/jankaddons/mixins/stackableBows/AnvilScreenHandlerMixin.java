2de6302409199f140059151c53639ca7e72e6a0f
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-09-17 01:30:00 -0700

    Fixed anvil dupe with stackable bows

--- src/main/java/com/crec0/jankaddons/mixins/stackableBows/AnvilScreenHandlerMixin.java
+++ src/main/java/com/crec0/jankaddons/mixins/stackableBows/AnvilScreenHandlerMixin.java
@@ -4,6 +4,7 @@ import com.crec0.jankaddons.JankAddonsSettings;
 import com.crec0.jankaddons.Utils;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.ForgingScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
@@ -21,12 +22,16 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
         super(type, syncId, playerInventory, context);
     }
 
-    @Inject(method = "updateResult", at = @At(value = "INVOKE", target = "net/minecraft/inventory/CraftingResultInventory.setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 4))
+    @Inject(method = "updateResult", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "net/minecraft/screen/AnvilScreenHandler.sendContentUpdates()V"))
     public void reduceToOne(CallbackInfo ci) {
-        ItemStack inputStack = this.input.getStack(0);
-        if (JankAddonsSettings.stackableFreshBows && !Utils.isFreshBow(inputStack)) {
-            ItemStack reducedInputStack = inputStack.copy();
-            reducedInputStack.setCount(inputStack.getCount() - 1);
+        ItemStack originalStack = this.input.getStack(0);
+        ItemStack mergeStack = this.input.getStack(1);
+        if (JankAddonsSettings.stackableFreshBows && Utils.isFreshBow(originalStack) && originalStack.getCount() > 1 && (mergeStack.isOf(Items.ENCHANTED_BOOK) || !Utils.isFreshBow(mergeStack))) {
+            ItemStack reducedInputStack = originalStack.copy();
+            originalStack.setCount(1);
+            this.input.setStack(0, originalStack);
+            this.output.getStack(0).setCount(1);
+            reducedInputStack.setCount(reducedInputStack.getCount() - 1);
             this.player.getInventory().offerOrDrop(reducedInputStack);
         }
     }
